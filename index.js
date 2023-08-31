406df2b98150402370bb97f59cbd45e77bff77c1
MichaelChemo2507 <mnh8551@gmail.com>
2023-08-31 20:33:30 +0300

    added a question CURD

--- index.js
+++ index.js
@@ -3,15 +3,71 @@ const port = 2507;
 const app = express();
 app.use(express.json());
 
+
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
 
 let db_M = require('./database');
 global.db_pool = db_M.pool;
 
-app.get("/",(req, res) => {
+app.post('/',(req,res)=>{
+    res.send("runing");
 });
-app.get("/List",(req, res) => {
+
+
+app.post("/AddQuestion",(req,res)=>{
+    let questObj = {};
+    questObj.questType = req.body.QuestType;
+    questObj.dimension = req.body.Dimension;
+    questObj.questContent = req.body.QuestContent;
+
+    const addQuery = `INSERT INTO question_id(Question_Type,Question_Text) VALUES(${questObj.questType},${questObj.dimension})`;
+    db_pool.query(addQuery,function (err,rows,fields){
+        if (err){
+            res.status(500).json({message:err});
+        }else{
+            res.status(200).json({fields:fields,rows:rows});
+        }
+    })
+    console.log(questObj);
+});
+app.post("/UpdateQuestion",(req ,res)=>{
+    let inx = req.body.index;
+    let questObj = {};
+    questObj.questType = req.body.QuestType;
+    questObj.dimension = req.body.Dimension;
+    questObj.questContent = req.body.QuestContent;
+
+    const addQuery = `UPDATE question_id SET Question_Type WHERE ID = ${inx}`;
+    db_pool.query(addQuery,function (err,rows,fields){
+        if (err){
+            res.status(500).json({message:err});
+        }else{
+            res.status(200).json({fields:fields,rows:rows});
+        }
+    })
+    res.send(`row - ${inx} updated`);
+    console.log(inx);
+})
+app.post("/DeleteQuestion",(req, res) => {
+    let inx = req.body.index;
+    const addQuery = `DELETE FROM question_id WHERE ID = ${inx}`;
+    db_pool.query(addQuery,function (err,rows,fields){
+        if (err){
+            res.status(500).json({message:err});
+        }else{
+            res.status(200).json({fields:fields,rows:rows});
+        }
+    })
+    res.send("row - "+ inx + " deleted");
+});
+
+app.get("/ReadQuestion",(req, res) => {
+    const addQuery = `SELECT * FROM question_id`;
+    db_pool.query(addQuery,function (err,rows,fields){
+        if (err) res.status(500).json({message: err});
+        else res.status(200).json({rows:rows});
+    })
 });
 app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
     console.log(`Now listening on port http://localhost:${port}`);
---
+++ student_info_db.sql
@@ -0,0 +1,97 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 29, 2023 at 11:58 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `student_info_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `question_id`
+--
+
+CREATE TABLE `question_id` (
+  `ID` int(11) NOT NULL,
+  `Question_Type` int(11) NOT NULL,
+  `Question_Text` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_answers`
+--
+
+CREATE TABLE `student_answers` (
+  `student_id` int(11) NOT NULL,
+  `question_id` int(11) NOT NULL,
+  `Answer_Int` int(11) NOT NULL,
+  `Answer_Str` varchar(250) NOT NULL,
+  `Answer_Time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_id`
+--
+
+CREATE TABLE `student_id` (
+  `ID` int(11) NOT NULL,
+  `First_Name` varchar(250) NOT NULL,
+  `Last_Name` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `question_id`
+--
+ALTER TABLE `question_id`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `student_id`
+--
+ALTER TABLE `student_id`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `question_id`
+--
+ALTER TABLE `question_id`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `student_id`
+--
+ALTER TABLE `student_id`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
