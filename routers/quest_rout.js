a8326939ed4ff984da1d7059a7bafd9adc37af99
MichaelChemo2507 <mnh8551@gmail.com>
2023-09-03 15:40:48 +0300

    added a question CURD

--- routers/quest_rout.js
+++ routers/quest_rout.js
@@ -9,11 +9,11 @@ router.post("/Add",(req,res)=>{
     questObj.questType = Number(req.body.QuestType);
     questObj.questContent = req.body.QuestContent;
     const addQuery = `INSERT INTO question_id(Category_id,Question_Type,Question_Text) VALUES(${questObj.category},${questObj.questType},'${questObj.questContent}')`;
-    db_pool.query(addQuery,function (err,rows,fields){
+    db_pool.query(addQuery,function (err,rows,fields,){
         if (err){
             res.status(500).json({message:err});
         }else{
-            res.status(200).json({fields:fields,rows:rows});
+            res.status(200).json({fields:fields,rows:rows,message:"OK",Last_Id:rows.insertId});
         }
     })
     console.log(questObj);
@@ -30,7 +30,7 @@ router.post("/Update",(req ,res)=>{
         if (err){
             res.status(500).json({message:err});
         }else{
-            res.status(200).json({fields:fields,rows:rows});
+            res.status(200).json({fields:fields,rows:rows,message:"OK"});
         }
     })
 })
@@ -41,14 +41,17 @@ router.post("/Delete",(req, res) => {
         if (err){
             res.status(500).json({message:err});
         }else{
-            res.status(200).json({fields:fields,rows:rows});
+            res.status(200).json({fields:fields,rows:rows,message:"OK"});
         }
     })
 
 });
 
 router.get("/List",(req, res) => {
-    const addQuery = `SELECT * FROM question_id`;
+    let categoryId = Number(req.body.cat_Id);
+    let addQuery = ``;
+    if ( categoryId > 0) addQuery = `SELECT * FROM question_id WHERE Category_id = ${categoryId}`;
+    else addQuery = `SELECT * FROM question_id`;
     db_pool.query(addQuery,function (err,rows,fields){
         if (err) res.status(500).json({message: err});
         else res.status(200).json({rows:rows});
