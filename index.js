bfb1b005ffbff7c2dcf1721d891e4290364ad5c2
Tomas545 <136482164+Tomas545@users.noreply.github.com>
2023-09-11 19:38:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 const express = require('express');
 const port = 2507;
 const app = express();
-app.use(express.json());
-
+const path = require('path');
 const bodyParser = require('body-parser');
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({extended: false}));
 
 let db_M = require('./database');
 global.db_pool = db_M.pool;
 
-app.get("/",(req, res) => {
-});
-app.get("/List",(req, res) => {
-});
+const quest_rtr = require('./routers/quest_rout');
+const front_rtr = require('./routers/fe_Checks_R');
+app.use('/Question', quest_rtr);
+app.use('/Front', front_rtr);
+
+const category_rtr = require('./routers/category_R');
+const Answers_rtr = require('./routers/Answers_R');
+app.use('/categories', category_rtr );
+app.use('/answers', Answers_rtr );
+
+
+//app.use('/Question', quest_rtr);
+//app.use('/Front', front_rtr);
+
 app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
     console.log(`Now listening on port http://localhost:${port}`);
 });
