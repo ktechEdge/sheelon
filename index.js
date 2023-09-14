4920fb7374e1e7ed9bb662e2ab67a2af4af21136
amird55 <amir.dgn@outlook.com>
2023-09-15 01:46:31 +0300

    adding back of Tom

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const port = 2507;
 const app = express();
 const path = require('path');
-
 const bodyParser = require('body-parser');
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
@@ -13,10 +12,8 @@ global.db_pool = db_M.pool;
 
 const quest_rtr = require('./routers/quest_rout');
 const front_rtr = require('./routers/fe_Checks_R');
-const users_rtr = require('./routers/users_rout');
 app.use('/Question', quest_rtr);
 app.use('/Front', front_rtr);
-app.use('/Users', users_rtr);
 
 app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
     console.log(`Now listening on port http://localhost:${port}`);
